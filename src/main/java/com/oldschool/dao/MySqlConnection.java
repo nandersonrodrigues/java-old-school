0fc29f891edd8d2865012e2601717fba526d0563
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-12-12 21:58:18 -0300

    Create class for connction with mysql

---
+++ src/main/java/com/oldschool/dao/MySqlConnection.java
@@ -0,0 +1,28 @@
+package com.oldschool.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MySqlConnection {
+	
+	public Connection getConnection() {
+		Connection conn = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/product", "teste", "teste");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+
+}
