533726b2d0e247ed67089ad9c8f045f9fc7186e2
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-12-12 21:52:27 -0300

    Change product atribute types

--- src/main/java/com/oldschool/model/Product.java
+++ src/main/java/com/oldschool/model/Product.java
@@ -1,57 +1,73 @@
 package com.oldschool.model;
 
+import com.oldschool.dao.ProductDao;
+
 public class Product {
 	
-	private int productId;
+	private Integer productId;
 	private String description;
-	private double price;
+	private Double price;
 	private boolean isAvailable;
+	private Integer quantity;
 	
 	public Product () {	}
 	
-	public Product(String description, double price, boolean isAvailable) {
+	public Product(String description, Double price, boolean isAvailable, Integer quantity) {
 		super();
 		this.description = description;
 		this.price = price;
 		this.isAvailable = isAvailable;
+		this.quantity = quantity;
 	}
 
-	public Product (int productId, String description, double price, boolean isAvailable) {
+	public Product (Integer productId, String description, Double price, boolean isAvailable) {
 		this.productId = productId;
 		this.description = description;
 		this.price = price;
 		this.isAvailable = isAvailable;
 	}
 
-	public int getProductId() {
+	public Integer getProductId() {
 		return productId;
 	}
-
-	public void setProductId(int productId) {
-		this.productId = productId;
-	}
-
+	
 	public String getDescription() {
 		return description;
 	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public double getPrice() {
+	
+	public Double getPrice() {
 		return price;
 	}
-
-	public void setPrice(double price) {
-		this.price = price;
+	
+	public Integer getQuantity() {
+		return quantity;
 	}
-
+	
 	public boolean isAvailable() {
 		return isAvailable;
+	}
+	
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}	
+
+	public void setPrice(Double price) {
+		this.price = price;
 	}
 
 	public void setAvailable(boolean isAvailable) {
 		this.isAvailable = isAvailable;
+	}
+	
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	
+	public void save() {
+		new ProductDao().registerProduct(this);
 	}
 }
