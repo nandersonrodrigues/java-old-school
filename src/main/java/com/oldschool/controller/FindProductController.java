6c8c111cfb336d675222bcae4f4036f97785f797
Nanderson Rodrigues <nandersondsr@gmail.com>
2021-12-20 09:52:58 -0300

    Create servlet to find products

---
+++ src/main/java/com/oldschool/controller/FindProductController.java
@@ -0,0 +1,37 @@
+package com.oldschool.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.oldschool.model.Product;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class FindProductController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public FindProductController() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String description = request.getParameter("description");
+		ArrayList<Product> products = new Product().findProductByDescription(description);
+		
+		for (Product product: products) {
+			System.out.println(product.getDescription());
+		}
+	}
+
+}
